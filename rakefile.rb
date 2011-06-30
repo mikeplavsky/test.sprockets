238cb0efede21ac5f4409f75ac00af61a2c56851
mikeplavsky <Mike.Plavsky@quest.com>
2011-06-30 21:47:37 +0400

    styling

--- rakefile.rb
+++ rakefile.rb
@@ -1,29 +1,27 @@
 desc "raw coffee script run"
 task :coffee do	
-
-	require "coffee_script"
-	puts CoffeeScript.compile File.read "test.coffee"
-	
+    require "coffee_script"
+    puts CoffeeScript.compile File.read "test.coffee"	
 end
 
 desc "run coffee script with tilt"
 task :tilt do	
 			
-	require 'tilt'
-	puts Tilt.new( 'test.coffee' ).render	
+    require 'tilt'
+    puts Tilt.new( 'test.coffee' ).render	
 	
 end
 
 desc "get everything with sprockets"
 task :sprockets do
 		
-	require 'sprockets'	
+    require 'sprockets'	
 	
-	env = Sprockets::Environment.new.tap do |e|
-		e.append_path '.'
-	end
+    env = Sprockets::Environment.new.tap do |e|
+        e.append_path '.'
+    end
 	
-	puts env[ 'application.js' ]
+    puts env[ 'application.js' ]
 		
 end
 
