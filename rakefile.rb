6815edd0c76a726776a841c093ed6820c960594f
mikeplavsky <Mike.Plavsky@quest.com>
2011-06-30 21:42:41 +0400

    init commit

---
+++ rakefile.rb
@@ -0,0 +1,30 @@
+desc "raw coffee script run"
+task :coffee do	
+
+	require "coffee_script"
+	puts CoffeeScript.compile File.read "test.coffee"
+	
+end
+
+desc "run coffee script with tilt"
+task :tilt do	
+			
+	require 'tilt'
+	puts Tilt.new( 'test.coffee' ).render	
+	
+end
+
+desc "get everything with sprockets"
+task :sprockets do
+		
+	require 'sprockets'	
+	
+	env = Sprockets::Environment.new.tap do |e|
+		e.append_path '.'
+	end
+	
+	puts env[ 'application.js' ]
+		
+end
+
+task :default => :sprockets
